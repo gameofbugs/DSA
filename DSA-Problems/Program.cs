ab5648daa901b769e03da54a33fe08aa6402ad83
gameofbugs <manojgamedevdeveloper@gmail.com>
2026-06-24 21:57:14 +0530

    Right Rotate by K Positions

--- DSA-Problems/Program.cs
+++ DSA-Problems/Program.cs
@@ -7,24 +7,24 @@ class Program
         int[] arr1 = { 1, 2, 3, 4, 5 };
         System.Console.WriteLine("Enter Number");
         int k = Convert.ToInt32(Console.ReadLine());
-        System.Console.WriteLine("Begpre Left ROtate");
+        System.Console.WriteLine("Begpre Right ROtate");
         foreach (int i in arr1)
         {
             System.Console.WriteLine(i);
         }
         for (int j = 0; j < k; j++)
         {
-            int firstElement = arr1[0];
-            for (int i = 1; i < arr1.Length; i++)
+            int lastElement = arr1[arr1.Length - 1];
+            for (int i = arr1.Length - 2; i >= 0; i--)
             {
-                arr1[i - 1] = arr1[i];
-                if (i == arr1.Length - 1)
+                arr1[i + 1] = arr1[i];
+                if (i == 0)
                 {
-                    arr1[arr1.Length - 1] = firstElement;
+                    arr1[i] = lastElement;
                 }
             }
         }
-        System.Console.WriteLine("After Left ROtate");
+        System.Console.WriteLine("After Right ROtate");
         foreach (int i in arr1)
         {
             System.Console.WriteLine(i);
